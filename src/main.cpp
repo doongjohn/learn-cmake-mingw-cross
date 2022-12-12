72b17a42a2aec76a000744d2b56367bf6d29ec71
doongjohn <doongjohn@gmail.com>
2022-12-13 01:20:35 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,66 @@
 
 #include <spdlog/spdlog.h>
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-HINSTANCE g_hInst;
-LPCTSTR lpszClass = TEXT("First");
+const char g_szClassName[] = "myWindowClass";
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
-  spdlog::info(fmt::format("I'd rather be {1} than {0}.", "right", "happy"));
-
-  HWND hWnd;
-  MSG msg;
-  WNDCLASS wndClass;
-  g_hInst = hInstance;
-
-  wndClass.cbClsExtra = 0;
-  wndClass.cbWndExtra = 0;
-  wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-  wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-  wndClass.hInstance = hInstance;
-  wndClass.lpfnWndProc = WndProc;
-  wndClass.lpszClassName = lpszClass;
-  wndClass.lpszMenuName = NULL;
-  wndClass.style = CS_HREDRAW | CS_VREDRAW;
-  RegisterClass(&wndClass);
-
-  hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-                      CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
-  ShowWindow(hWnd, nCmdShow);
-
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
-  return (int)msg.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
-  switch (iMessage) {
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+  switch (msg) {
+  case WM_CLOSE:
+    DestroyWindow(hwnd);
+    break;
   case WM_DESTROY:
     PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+  }
+  return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  spdlog::info(fmt::format("I'd rather be {1} than {0}.", "right", "happy"));
+
+  WNDCLASSEX wc;
+  HWND hwnd;
+  MSG Msg;
+
+  // Step 1: Registering the Window Class
+  wc.cbSize = sizeof(WNDCLASSEX);
+  wc.style = 0;
+  wc.lpfnWndProc = WndProc;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInstance;
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wc.lpszMenuName = NULL;
+  wc.lpszClassName = g_szClassName;
+  wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+  if (!RegisterClassEx(&wc)) {
+    MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
     return 0;
   }
-  return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+
+  // Step 2: Creating the Window
+  hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, g_szClassName, "The title of my window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                        CW_USEDEFAULT, 600, 300, NULL, NULL, hInstance, NULL);
+
+  if (hwnd == NULL) {
+    MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+  }
+
+  ShowWindow(hwnd, nCmdShow);
+  UpdateWindow(hwnd);
+
+  // Step 3: The Message Loop
+  while (GetMessage(&Msg, NULL, 0, 0) > 0) {
+    TranslateMessage(&Msg);
+    DispatchMessage(&Msg);
+  }
+
+  return Msg.wParam;
 }
