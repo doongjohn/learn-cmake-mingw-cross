48c85c6ba73a141b6336b21eca9f57930c2e783c
doongjohn <doongjohn@gmail.com>
2022-12-12 15:57:23 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <windows.h>
 
 #include <spdlog/spdlog.h>
-#include <fmt/core.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 HINSTANCE g_hInst;
@@ -12,31 +11,31 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
   spdlog::info(fmt::format("I'd rather be {1} than {0}.", "right", "happy"));
 
   HWND hWnd;
-  MSG Message;
-  WNDCLASS WndClass;
+  MSG msg;
+  WNDCLASS wndClass;
   g_hInst = hInstance;
 
-  WndClass.cbClsExtra = 0;
-  WndClass.cbWndExtra = 0;
-  WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-  WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-  WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-  WndClass.hInstance = hInstance;
-  WndClass.lpfnWndProc = WndProc;
-  WndClass.lpszClassName = lpszClass;
-  WndClass.lpszMenuName = NULL;
-  WndClass.style = CS_HREDRAW | CS_VREDRAW;
-  RegisterClass(&WndClass);
+  wndClass.cbClsExtra = 0;
+  wndClass.cbWndExtra = 0;
+  wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+  wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wndClass.hInstance = hInstance;
+  wndClass.lpfnWndProc = WndProc;
+  wndClass.lpszClassName = lpszClass;
+  wndClass.lpszMenuName = NULL;
+  wndClass.style = CS_HREDRAW | CS_VREDRAW;
+  RegisterClass(&wndClass);
 
   hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
                       CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
   ShowWindow(hWnd, nCmdShow);
 
-  while (GetMessage(&Message, NULL, 0, 0)) {
-    TranslateMessage(&Message);
-    DispatchMessage(&Message);
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
   }
-  return (int)Message.wParam;
+  return (int)msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
